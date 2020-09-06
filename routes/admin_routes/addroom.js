639046c1f308ec8d153e096ac261d978e8630742
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-06 19:29:28 +0530

    updated admin route

--- routes/admin_routes/addroom.js
+++ routes/admin_routes/addroom.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+const room = require("../../models/room");
 
 router.post("/addroom",async (req,res) =>{
     try{
-        
+        const roomno = req.body.roomno;
+        let data = await room.findOne({roomno});
+        if(data){
+            res.send({
+                message: "Room already exists"
+            });
+        }
+        else{
+            
+        }
     }
     catch(err){
         res.status(500).send({
