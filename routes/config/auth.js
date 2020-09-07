def11d12efbc10f499eff77a7a52368798dc566b
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-07 20:01:38 +0530

    fixed auth file

--- routes/config/auth.js
+++ routes/config/auth.js
@@ -2,13 +2,15 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req,res,next) => {
     try{
-        let token = req.body.token;
+        let token = req.headers.authorization.split(" ")[1];
         if(!token){
             return res.status(401).send({
                 message: "Unauthorized"
             });
         }
-        let user = await jwt.verify(value,"123");
+        let user = await jwt.verify(token,"123");
+        req.userdata = user;
+        next();
     }
     catch(err){
         res.status(500).send({
