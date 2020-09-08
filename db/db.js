25b945f1b5bf6a848681e21b7d0a5b147b63defe
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-08 14:41:14 +0530

    fixed bugs

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 function db(){
     mongoose.connect("mongodb://localhost/hostel_booking",{
         useNewUrlParser: true,
+        useFindAndModify: false,
         useUnifiedTopology: true
     })
     .then(() => {
